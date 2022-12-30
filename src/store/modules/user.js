17f312af144dadce169e558cd571efffc234c000
danielcrubens <dcr.tecnologi@gmail.com>
2022-12-30 19:36:04 -0300

    info localStorage  :memo:

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -12,11 +12,30 @@ const mutations = {
     state.password = payload.password;
     state.isLoggedIn = true;
   },
+
+  LOG_OUT(state) {
+    state.name = "";
+    state.password = "";
+    state.isLoggedIn = false;
+  },
 };
 
 const actions = {
   authentication(context, user) {
     context.commit("AUTHENTICATION", user);
+    localStorage.setItem("user", JSON.stringify(user));
+  },
+
+  logout(context) {
+    context.commit("LOG_OUT");
+    localStorage.removeItem("user");
+  },
+
+  checkSession(context) {
+    const user = localStorage.getItem("user");
+    if (user) {
+      context.commit("AUTHENTICATION", JSON.parse(user));
+    }
   },
 };
 
