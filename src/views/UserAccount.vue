fb1f1bec397b2fd7b75ccd2ac017c3c334c5c425
danielcrubens <dcr.tecnologi@gmail.com>
2022-12-30 19:28:38 -0300

    usuário conectado  :memo:

--- src/views/UserAccount.vue
+++ src/views/UserAccount.vue
@@ -1,11 +1,19 @@
 <template>
   <div>
-    Account user
+   <h1> Olá, {{ name }}</h1>
+
   </div>
 </template>
 
 <script>
+import { useStore } from "vuex";
+
 export default {
-  
-}
+  setup() {
+    const store = useStore();
+    return {
+      name: store.state.user.name,
+    };
+  },
+};
 </script>
