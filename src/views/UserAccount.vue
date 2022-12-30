428fb26a4587fab4fd137c28a3e9606acf0c8e79
danielcrubens <dcr.tecnologi@gmail.com>
2022-12-30 19:31:28 -0300

    criando ação de logout  :memo:

--- src/views/UserAccount.vue
+++ src/views/UserAccount.vue
@@ -1,18 +1,27 @@
 <template>
   <div>
+<button @click="logout()"> Logout</button>
    <h1> Olá, {{ name }}</h1>
-
   </div>
 </template>
 
 <script>
+import router from "@/router";
+
 import { useStore } from "vuex";
 
 export default {
   setup() {
     const store = useStore();
+
+    const logout = () => {
+      store.dispatch("user/logout");
+      router.push({ name: "Home" });
+    };
+
     return {
       name: store.state.user.name,
+      logout,
     };
   },
 };
