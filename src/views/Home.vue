68a1b5f430b903e3b2bf2d32cfcf322c6c1c591b
danielcrubens <dcr.tecnologi@gmail.com>
2022-12-30 20:01:41 -0300

    regra ação condicional  :memo:

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,13 @@
 <template>
   <div>
+
+    <p>Encerrar {{ action }}</p>
+
     <ul>
-      <li>
+      <li v-if="action === 'sign in'">
         <router-link to="/account">Your Account</router-link>
       </li>
-      <li>
+      <li v-else>
         <router-link to="/signin">Sign In</router-link>
       </li>
     </ul>
@@ -13,7 +16,18 @@
 </template>
 
 <script>
+import { useStore } from "vuex";
+import { computed } from "vue";
+
 export default {
-  name: 'Home',
-}
+  name: "Home",
+  setup() {
+    const store = useStore();
+    return {
+      action: computed(() =>
+        store.state.user.isLoggedIn ? "access" : "sign in"
+      ),
+    };
+  },
+};
 </script>
