8e6ea90289db04b5eb57a1c864ac3798315f7e1f
danielcrubens <dcr.tecnologi@gmail.com>
2022-12-30 19:34:43 -0300

    check localStorage  :memo:

--- src/App.vue
+++ src/App.vue
@@ -5,10 +5,15 @@
 
 <script>
 import Header from "@/components/TheHeader.vue";
+import store from "@/store";
+import { onMounted } from "vue";
+
 export default {
   components: { Header },
+  setup() {
+    onMounted(() => store.dispatch("user/checkSession"));
+  },
 };
-</script>
 
 
 
