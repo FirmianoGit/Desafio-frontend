6409d6eb189f50dbd5742d16cb5ed9f88900a4c7
danielcrubens <dcr.tecnologi@gmail.com>
2022-12-30 17:27:17 -0300

    components header :memo:

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,14 @@
 <template>
-<router-view ></router-view>
+  <Header title="VueLogin" home="/" />
+  <router-view />
 </template>
+
+<script>
+import Header from "@/components/TheHeader.vue";
+export default {
+  components: { Header },
+};
+</script>
 
 
 
